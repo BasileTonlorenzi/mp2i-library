93f4f5e876e2c063ac0e9e328185be467f82f2a6
BasileTonlorenzi <90552930+BasileTonlorenzi@users.noreply.github.com>
2021-11-06 19:50:18 +0100

    Update tableau_dynamique.ml

--- tableau_dynamique.ml
+++ tableau_dynamique.ml
@@ -45,19 +45,6 @@ for i = 0 to (d2.n) do
 done;
 d3;;
 
-(* Fonction coupant un tableau dynamique en 2 à la position m*)
-let split d m =
-(*On définie d'abord les tableaux dynamiques à modifier*)
-let d1 = {t = Array.make m d.t.(0); n = m-1} in
-let difference = Array.length d.t - Array.length d1.t in
-let d2 = {t = Array.make difference d.t.(0); n = d.n - (m-1)} in
-
-for i = 0 to Array.length d.t do
-    if i <= (m-1) then d1.t.(i) <- d.t.(i)
-    else d2.t.(i) <- d.t.(i)
-done;
-(d1, d2);; (*d1 a tout les élément jusque m, m compris, d2 a tout les autres éléments*)
-
 (* Fonction permettant de supprimer un élément à une position précise d'un tableau *)
 let suppr d m =
 let d1 = {t = [| |]; n = 0} in
@@ -76,23 +63,4 @@ for i = 0 to (d.n - 1) do
 done;
 d1;;
 
-(* Inverser les ordres des éléments d'un tableau *)
-let reverse d =
-    for i = 0 to (d.n)/2 do
-      let a = d.t.(i) in (* On pose une variable intermédiare pour échanger 2 à 2 les éléments du tableau *)
-      d.t.(i) <- d.t.(d.n - i);
-      d.t.(d.n - i) <- a;
-    done;
-d;;
-
-(* Recherche d'élément par dichotomie *)
-let dichotomie d e =
-  let rec aux i j =
-    if i > j then false
-    else
-    let m = (i + j)/2 in
-    if d.t.(m) = e then true
-    else if d.t.(m) < e then aux (m-1) j
-    else aux i (m+1)
- in aux 0 (d.n - 1)
 
